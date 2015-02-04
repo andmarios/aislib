161b4df6799afe09b3bca0197952e748e97f94a4
Marios Andreopoulos <opensource@andmarios.com>
2015-02-04 22:44:43 +0200

    formatting and documentation

--- example/example-tcp.go
+++ example/example-tcp.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-// Here are saved as JSON the ships seen in the last 5 seconds.
+// Here are saved as JSON string the ships seen in the last 5-second period.
 var serveJSON string
 
 type shipData struct {
@@ -22,15 +22,13 @@ type shipData struct {
 
 func main() {
 
-	// Create an AIS router to decode radio sentences
+	// Create an AIS router process to decode radio sentences
 	send := make(chan string, 1024)
 	receive := make(chan ais.Message, 1024)
 	failed := make(chan ais.FailedSentence, 1024)
-
 	go ais.Router(send, receive, failed)
 
-	// Create a handler that reads messages from router, decodes and saves the payload
-
+	// Create a handler-process that reads messages from router, decodes and saves the payload
 	seen := make(map[uint32]shipData)
 	proceed := make(chan bool)
 	go func() {
@@ -51,7 +49,7 @@ func main() {
 		}
 	}()
 
-	// Create a function that every five seconds refreshes [serveJSON] with new data
+	// Create a process that every five seconds refreshes [serveJSON] with new data
 	go func() {
 		var jsonBuf bytes.Buffer
 		for _ = range time.Tick(5 * time.Second) {
@@ -78,12 +76,10 @@ func main() {
 	// Connect to a remote AIS server. Read AIS sentences and forward them to the AIS router.
 	remote := "ais1.shipraiser.net:6492"
 	serverAddr, err := net.ResolveTCPAddr("tcp", remote)
-
 	if err != nil {
 		log.Println(err)
 	}
 	conn, err := net.DialTCP("tcp", nil, serverAddr)
-
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -97,12 +93,14 @@ func main() {
 		}
 	}()
 
+	// Create a server to listen for files/data requests
 	http.HandleFunc("/data", dataHandler)
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8080", nil)
 
 }
 
+// Function to serve the ships JSON string
 func dataHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", serveJSON)
 }
