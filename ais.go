4efa346d721792eab240c15ef5da5304275ec877
Marios Andreopoulos <opensource@andmarios.com>
2015-02-04 17:12:46 +0200

    OCD: tiny doc fix

--- ais.go
+++ ais.go
@@ -1,4 +1,4 @@
-// ais provides functions and types to work with AIS (Automatic Identification System) sentences (radio messages) and messages in AIVDM/AIVDO format.
+// Package ais provides functions and types to work with AIS (Automatic Identification System) sentences (radio messages) and messages in AIVDM/AIVDO format.
 //
 // An AIS sentence is one line, it is the actual radio message.
 // An AIS message is the payload that is carried by one or more consecutive AIS sentences.
